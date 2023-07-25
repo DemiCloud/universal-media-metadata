737386c5c8d140ee4e96a090f3115f3cdf8326d7
SomeHybrid <91648368+SomeHybrid@users.noreply.github.com>
2023-07-25 13:49:10 +0700

    refactor: removed os.path in favor of pathlib

--- umm/__init__.py
+++ umm/__init__.py
@@ -14,42 +14,33 @@ from pathlib import Path
 import typer
 from rich.console import Console
 
-import umm._vars as Vars
-import umm.file as File
+from umm._vars import Constants
+from umm._vars import Variable
+from umm.file import cmd
+
 # endregion
 
 # region Typer Definitions
-cmd = typer.Typer(
-  rich_markup_mode="rich",
-  no_args_is_help=True
-)
-cmd.add_typer(File.cmd, name="file")
+app = typer.Typer(rich_markup_mode="rich", no_args_is_help=True)
+app.add_typer(cmd, name="file")
 console = Console()
 # endregion
 
-@cmd.command("version")
-def cmd_version() -> None:
-  """Print version and exit
-  """
-  print(f"{Vars.Constants.NAME}: Version {__version__}")
 
-@cmd.callback()
+@app.command("version")
+def app_version() -> None:
+    """Print version and exit"""
+    print(f"{Constants.NAME}: Version {__version__}")
+
+
+@app.callback()
 def main() -> None:
-  """ CLI tool to manage Netbox """
-  Vars.Variable.proj_dir = Path(os.path.dirname(os.path.realpath(__file__)))
+    """CLI tool to manage Netbox"""
+    Variable.proj_dir = Path(__file__).parent.resolve()
 
-def cli(
-  app_name: str = "umm-cli"
-):
-  """ Entry Point for the CLI Application"""
-  Vars.Variable.app_name = app_name
-  Vars.Variable.rel_dir = Path(
-    os.path.abspath(
-      os.path.expanduser(
-        os.path.dirname(
-          inspect.stack()[1].filename
-        )
-      )
-    )
-  )
-  cmd()
+
+def cli(app_name: str = "umm-cli"):
+    """Entry Point for the CLI Application"""
+    Variable.app_name = app_name
+    Variable.rel_dir = Path(inspect.stack()[1].filename).parent.resolve()
+    app()
--- umm/schema/__init__.py
+++ umm/schema/__init__.py
@@ -4,7 +4,6 @@
 import datetime
 import importlib
 import json
-import os
 from pathlib import Path
 
 from jsonschema import Draft202012Validator, validators
@@ -13,73 +12,70 @@ from packaging import version
 
 # endregion
 
-umm_checker= Draft202012Validator.TYPE_CHECKER.redefine_many(
-  {
-    "version": lambda _, instance: isinstance(instance, version.Version),
-    "path": lambda _, instance: isinstance(instance, Path),
-    "date": lambda _, instance: isinstance(instance, datetime.date),
-  }
+umm_checker = Draft202012Validator.TYPE_CHECKER.redefine_many(
+    {
+        "version": lambda _, instance: isinstance(instance, version.Version),
+        "path": lambda _, instance: isinstance(instance, Path),
+        "date": lambda _, instance: isinstance(instance, datetime.date),
+    }
 )
 
-UmmValidator = validators.extend(Draft202012Validator, type_checker=umm_checker)
+UmmValidator = validators.extend(
+    Draft202012Validator,
+    type_checker=umm_checker
+)
+
 
 def validate(
-  umm: dict,
+    umm: dict,
 ) -> None:
-  """Validate UMM
-  """
-  umm_version = umm['info']['version']
-  schema_file = Path(
-    os.path.join(
-      "./umm/assets/schemas/",
-      umm['info']['class'],
-      f"{str(umm_version)}.schema"
+    """Validate UMM"""
+    umm_version = umm["info"]["version"]
+    schema_dir = Path("./umm/assets/schemas/") / umm["info"]["class"]
+    schema_file = schema_dir / f"{str(umm_version)}.schema"
+    schema_file = (
+        importlib.resources.files("umm")
+        / "assets"
+        / "schemas"
+        / umm["info"]["class"]
+        / f"{str(umm_version)}.schema"
     )
-  )
-  schema_file = (
-    importlib.resources.files("umm") /
-    "assets" /
-    "schemas" /
-    umm['info']['class'] /
-    f"{str(umm_version)}.schema"
-  )
-  with open(schema_file,"rb") as file:
-    schema = json.load(file)
-  validator = UmmValidator(schema)
-  try:
-    validator.validate(umm)
-    return True
-  except ValidationError:
-    return False
-
-def normalize(
-  umm: dict,
-  umm_path: Path
-) -> dict:
-  """normalized the metadata dictionary
-
-  Args:
-      umm (dict): metadata dictionary
-
-  Returns:
-      dict: normalized metadata dictionary
-  """
-  try:
-    umm['info']['version'] = version.parse(umm['info']['version'])
-  except version.InvalidVersion:
-    print(f"{umm['info']['version']} is not a valid version number")
-
-  if not isinstance(umm['metadata']['date'],datetime.date):
-    print(f"{umm['metadata']['date']} is not a valid date format. Date should be YYYY-MM-DD")
-
-  if os.path.isabs(os.path.abspath(os.path.expanduser(umm['metadata']['cover']))):
-    umm['metadata']['cover'] = os.path.abspath(os.path.expanduser(umm['metadata']['cover']))
-  else:
-    umm['metadata']['cover']= os.path.join(umm_path,umm['metadata']['cover'])
+    with open(schema_file, "rb") as file:
+        schema = json.load(file)
+    validator = UmmValidator(schema)
+    try:
+        validator.validate(umm)
+        return True
+    except ValidationError:
+        return False
 
 
-  try:
-    umm['metadata']['cover'] = Path(umm['metadata']['cover'])
-  except TypeError:
-    print("metadata.cover should be a valid path")
-  return umm
+def normalize(umm: dict, umm_path: Path) -> dict:
+    """normalized the metadata dictionary
+
+    Args:
+        umm (dict): metadata dictionary
+
+    Returns:
+        dict: normalized metadata dictionary
+    """
+    try:
+        umm["info"]["version"] = version.parse(umm["info"]["version"])
+    except version.InvalidVersion:
+        print(f"{umm['info']['version']} is not a valid version number")
+
+    if not isinstance(umm["metadata"]["date"], datetime.date):
+        print(
+            f"{umm['metadata']['date']} is not a valid date format. Date should be YYYY-MM-DD"
+        )
+
+    if Path(umm["metadata"]["cover"]).is_absolute():
+        umm["metadata"]["cover"] = (umm_path / umm["metadata"]["cover"]).resolve()
+    else:
+        umm["metadata"]["cover"] = umm_path / umm["metadata"]["cover"]
+
+    try:
+        umm["metadata"]["cover"] = Path(umm["metadata"]["cover"])
+    except TypeError:
+        print("metadata.cover should be a valid path")
+    return umm
